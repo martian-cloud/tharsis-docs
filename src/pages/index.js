f3c740529b869ce2d678b8b6841235d8cb9b2da1
Brandon Breuil <brandon.breuil@gmail.com>
2024-11-04 15:22:31 +0000

    Add initial blog announcing tharsis as an oss terraform platform

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,65 @@
-import React from "react";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import HeaderCards from "./header_cards.tsx";
+import Layout from "@theme/Layout";
+import React from "react";
+import ReactPlayer from 'react-player';
 import styles from "./index.module.css";
 
+function DemoVideo() {
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <ReactPlayer
+            config={{
+                file: {
+                    attributes: {
+                        style: {
+                            width: "100%",
+                            height: "100%",
+                            display: "block",
+                            borderRadius: "0.5rem"
+                        }
+                    }
+                }
+            }}
+            width="100%"
+            height="100%"
+            playing
+            muted
+            loop
+            controls
+            url={`${siteConfig.baseUrl}img/demo.mp4`}
+        />
+    );
+}
+
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={"hero hero--primary"}>
-      <div id={styles.parent}>
-        <div className="container">
-          <h1 className={styles.heroTitle}>{siteConfig.title}</h1>
-          <p className={styles.heroSubtitle}>{siteConfig.tagline}</p>
-          <p id={styles.description}>
-            A remote Terraform backend that provides state management and a full
-            execution environment for running Terraform modules. Tharsis
-            includes additional capabilities to facilitate the management of
-            Terraform workspaces within an organization.
-          </p>
-        </div>
-        <HeaderCards />
-      </div>
-    </header>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <header className={"hero hero--primary"}>
+            <div className={styles.heroContent}>
+                <div className={styles.heroBanner}>
+                    <h1 className={styles.heroTitle}>
+                        {siteConfig.tagline}
+                    </h1>
+                    <p className={styles.heroSubtitle}>
+                        Tharsis is an enterprise scale Terraform platform that offers a complete solution for managing your Terraform deployments, state and workspaces
+                    </p>
+                </div>
+                <div className={styles.demoVideo}>
+                    <DemoVideo />
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default function Home() {
-  return (
-    <Layout title={`Documentation`} description="Tharsis Documentation site">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+    return (
+        <Layout title={`Documentation`} description="Tharsis Documentation site">
+            <HomepageHeader />
+            <main>
+                <HomepageFeatures />
+            </main>
+        </Layout>
+    );
 }
